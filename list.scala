275d267fd261bbfde2dc7a033fe5afb15ec020ee
natsumisawa <natsumi.sawa@bizreach.co.jp>
2020-06-08 22:25:18 +0900

    ✨

--- list.scala
+++ list.scala
@@ -1,8 +1,9 @@
 object List {
   def main(args: Array[String]): Unit = {
     // 降順にする
-    val optionList = Seq(Some(1), Some(3), Some(2), None)
+    val optionList = Seq(Some(1), Some(3), None, Some(2))
     println(optionList.sorted.reverse)
+    println(optionList.sortBy(x => x).reverse)
     println(optionList.sortBy(x => x.getOrElse(0)).reverse)
     println(
       optionList.sortWith(
